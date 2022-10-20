2c03056b120048291c66915a5105ab3f672eb791
Srdan Popara <srdan.popara@yahoo.com>
2022-10-20 09:47:40 +0200

    Add custom button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,19 @@
+<button on:click>
+    <slot />
+</button>
+
+<style lang="scss">
+    button {
+        padding: 8px 12px;
+        background-color: #ffcc74;
+        color: #000;
+        font-weight: bold;
+        border: none;
+        border-radius: 3px;
+        cursor: pointer;
+    }
+
+    button:hover {
+        color: #fff;
+    }
+</style>
